91897f3cf35f858619137d6edb35035a8d09453a
li-cheng-guo <1450579753@qq.com>
2018-12-16 17:21:43 +0800

    解析影片信息

--- src/edu/csuft/glcheng/spider/App.java
+++ src/edu/csuft/glcheng/spider/App.java
@@ -1,12 +1,71 @@
 package edu.csuft.glcheng.spider;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * 程序的入口
  * 
- * @author glcheng
- * @author other
+ * @author Y
  *
  */
 public class App {
-
+	
+	// alt + / : 提示
+	
+	public static void main(String[] args) {
+		
+		// 目标 URL
+		String url = "https://movie.douban.com/top250";
+		
+		// 使用 JSOUP 抓取文档
+//		try {
+//			Document doc = Jsoup.connect(url).get();
+//			
+//			Elements es = doc.select(".grid_view a+span");
+//			System.out.println(es.size());
+//			
+//			for (Element e : es) {
+//				System.out.println(e);
+//			}
+			
+			try {
+				Document doc = Jsoup.connect(url).get();
+				
+				Elements es = doc.select(".grid_view .item");
+				System.out.println(es.size());
+				
+				// 创建一个影片的列表
+				ArrayList<Film> list = new ArrayList<>();
+				
+				for (Element e : es) {
+					Film f = new Film();
+					// 每一部影片
+					Element t = e.select(".title").first();
+					String num = e.select(".star span").last().text();
+					System.out.println(t.text() + ", " + num.subSequence(0, num.length()-3));
+				
+//					f.id;
+//					f.title;
+					list.add(f);
+				}
+			
+//			String title = doc.title();
+//			String data = doc.data();
+//			
+//			System.out.println(title);
+//			System.out.println(data);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
---
+++ src/edu/csuft/glcheng/spider/Film.java
@@ -0,0 +1,5 @@
+package edu.csuft.glcheng.spider;
+
+public class Film {
+
+}
